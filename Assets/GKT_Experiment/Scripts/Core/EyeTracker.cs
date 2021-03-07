f3498e0caea82068fc620ff3c26a7acef679c90f
Eric Zhan <doremi31618@gmail.com>
2021-03-07 17:14:43 +0800

    Add Eye Tracker

---
+++ Assets/GKT_Experiment/Scripts/Core/EyeTracker.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PupilLabs;
+public class EyeTracker : MonoBehaviour
+{
+    public RecordingController recordingController;
+    [Header("Recording Data")]
+    public string savingPath;
+
+    [Header("Annotations")]
+    public AnnotationPublisher annotationPub;
+    public Transform eyePosition;
+
+    void Start()
+    {
+
+    }
+
+    void Setup()
+    {
+        recordingController.useCustomPath = true;
+    }
+    public void SendTimeStamp()
+    {
+        Dictionary<string, object> userInfo = new Dictionary<string, object>();
+
+        // userInfo["head_world_x"] = head.position.x;
+        // userInfo["head_world_y"] = head.position.y;
+        // userInfo["head_world_z"] = head.position.z;
+
+        annotationPub.SendAnnotation(label: "Notice Target", customData: userInfo);
+    }
+    public void SavingTrialData(int index, string path)
+    {
+        recordingController.SetCustomPath(path);
+
+    }
+
+}
